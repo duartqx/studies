e740dfdf8086e7394501156eed404b62798ca5c8
Diego Duarte <duartqx@gmail.com>
2024-06-01 21:08:48 -0300

    alison-spring-1 init

---
+++ alison/spring-1/src/test/java/com/duartqx/alison_spring_1/AlisonSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duartqx.alison_spring_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlisonSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
