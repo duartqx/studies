7392417251a773b0a8eb1cbb07f202acbd5f6f62
Diego Duarte <duartqx@gmail.com>
2024-06-08 19:44:55 -0300

    alisonspring

---
+++ alison/spring-1/src/main/java/com/duartqx/alison_spring_1/entity/Product.java
@@ -0,0 +1,64 @@
+package com.duartqx.alison_spring_1.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String category;
+    private String type;
+    private String sector;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Product setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Product setCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Product setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public Product setSector(String sector) {
+        this.sector = sector;
+        return this;
+    }
+}
