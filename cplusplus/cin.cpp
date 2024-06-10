73ebb4afca234d331b659bba72242e973c6c60c1
Diego Duarte <duartqx@gmail.com>
2024-06-09 22:57:34 -0300

    cin.cpp

---
+++ cplusplus/cin.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    int in;
+    std::cout << "Digite um valor: ";
+    std::cin >> in;
+    std::cout << "Você definiu o valor: " << in << "\n";
+    return 0;
+}
