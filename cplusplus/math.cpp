3e3fc33a229b069e44a2c726d30f63e9508d1f74
Diego Duarte <duartqx@gmail.com>
2024-06-09 22:01:32 -0300

    math.cpp

---
+++ cplusplus/math.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int a, b, result;
+
+    a = 5;
+    b = 3;
+
+    a++;
+
+    result = a + b;
+
+    std::cout << "Resultado: ";
+    std::cout << result;
+    std::cout << "\n";
+
+    return 0;
+}
