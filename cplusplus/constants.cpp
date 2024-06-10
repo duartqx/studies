0aaf218cdd8d3c6f7a877d90fbb0fcacdc8b6dae
Diego Duarte <duartqx@gmail.com>
2024-06-09 22:57:22 -0300

    constants.cpp

---
+++ cplusplus/constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+const float pi = 3.14159;
+const char newline = '\n';
+
+int main() {
+    double r = 5.0;
+    double circle;
+
+    circle = pi * 2 * r;
+    std::cout << "Circle: " << circle << newline;
+}
